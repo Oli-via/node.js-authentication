768c6b51335c3a589fe34af0b508fe081390d25f
Olivia Wang <aoli.wl@alibabapictures.com>
2017-01-23 19:29:20 +0800

    mongoose setup

--- models/user.js
+++ models/user.js
@@ -2,4 +2,20 @@
  * Created by aliyy on 2017/1/23.
  */
 const mongoose = require('mongoose')
+// Schema is what we use to tell mongoose about the very particular fields that our model is going to have
 const Schema = mongoose.Schema
+
+// Define our model
+const userSchema = new Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: String
+})
+
+
+
+// Create the model class
+const ModelClass = mongoose.model('user', userSchema)    // loads the Schema into mongoose
+
+
+// Export the model
+module.exports = ModelClass
