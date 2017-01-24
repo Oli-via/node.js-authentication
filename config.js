78a28d88060858abf94619322623c1310ca61b5b
Olivia Wang <aoli.wl@alibabapictures.com>
2017-01-24 11:17:57 +0800

    jwt encode

---
+++ config.js
@@ -0,0 +1,5 @@
+//  Hold application secrets and config
+
+module.exports = {
+  secret: 'sfijsuf9suejiw3rr3i'
+}
--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,13 +1,24 @@
+const jwt = require('jwt-simple')
+const config = require('../config')
 const User = require('../models/user')
 
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  // 将第一个参数中的内容用第二个参数来编码
+  // as a convention, jwt has a sub property, which is short for subject
+  // subject meaning, who is the token belong to
+  // the subject(key) of this token, is this user(value)
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+//  iat is another jwt property, short for issue add time
+}
+
 exports.signUp = function (req, res, next) {
-  
   // req.body: 得到post请求body中的内容
   const email = req.body.email;
   const password = req.body.password;
 
   if (!email || !password) {
-    return res.status(422).send({ error: 'You must provide email and password'})
+    return res.status(422).send({ error: 'You must provide email and password '})
   }
   
 //  See if a user with the given email exists
@@ -33,7 +44,7 @@ exports.signUp = function (req, res, next) {
       }
 
       //  Respond to request indicating the user was created
-      res.json({ success: true});
+      res.json({ token: tokenForUser(user)});
     })
   })
 }
